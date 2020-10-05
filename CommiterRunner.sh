7c44992698382c73e61a153c8ddf27bab30d2334
Elisha Hollander <just4now666666@gmail.com>
2020-10-05 16:27:21 +0300

    Update CommiterRunner.sh

--- CommiterRunner.sh
+++ CommiterRunner.sh
@@ -1,26 +1,6 @@
 #!/bin/bash 
-
-cd Commiter 
-
-while true ; do
-for (( j=0; j <= 10; ++j ))
-do
-
-for (( i=0; i <= 10000; ++i ))
-do
-./deploy.sh
-done 
-
-git push origin master
-done
-cd ..
-rm -rf ./Commiter 
-git clone --depth 1 git@github.com:DefCon-007/Commiter.git
-cd Commiter 
-git config user.name "ABC"
-git config user.email "abc@xyz.com"
-done
-
-
-
-
+git config core.autocrlf false
+for (( j=0; j <= 10; ++j )) ; do
+for (( i=0; i <= 1000; ++i )) ; do
+echo ".">>dots && git add . && git commit -m "." ; done 
+rm dots && git add . && git commit -m "." && git push origin master ; done
